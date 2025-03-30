3bb8ba4c3cb502fd9439c3fb028db8def0596580
chc1129 <chc1129@gmail.com>
2025-03-30 19:06:52 +0900

    code fix

---
+++ 03-P02/practice3_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 2022;
+    if (a % 3 == 0)
+    {
+        printf("3で割り切れます\n");
+    }
+    else
+    {
+        printf("3で割り切れません\n");
+    }
+
+}
